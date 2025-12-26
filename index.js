f29b2a78e3300526e9ca6477a29e86fd0e70d772
Richit <richit@local.dev>
2025-12-26 09:42:30 +0530

    Refactor index.js to enhance stats logging and add action outputs; create new frontend files for future development

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const { decEnergy, incEnergy } = require('./src/energy')
-const {incHunger, decHunger} = require('./src/hunger')
-const { food } = require('./src/food')
-const { get } = require('./src/stats')
-const { applyTimeReward } = require('./src/timeReward')
-const { rewardEffort } = require('./src/reward')
-const { actions } = require('./src/actions')
-console.log('Current Stats:', get())
+const { get } = require("./src/stats")
+const { hungerTick } = require("./src/hungerTick")
+const { applyTimeReward } = require("./src/timeReward")
+const { actions } = require("./src/actions")
+const { food } = require("./src/food")
+
+applyTimeReward()
+hungerTick()
+
+console.log("Current Stats:", get())
 
 const act = actions()
 const eat = food()
+
+console.log(act.sleep())
+console.log(eat.apple())
+console.log(act.play())
+
+console.log("Updated Stats:", get())
+ 
